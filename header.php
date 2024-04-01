e0e413c611f82fd6813fb9e7dae523848cbb9bea
JoaoAntonio <joaoantoniosantosalves1@gmail.com>
2024-04-01 11:10:26 -0300

    começo-Layout

---
+++ header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Backups</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css">
+  </head>
+  <body>
+    <div
+    class="container"
+    ><h1>Backups</h1>
+  <!--  <img class="arrow" src="img/download-da-nuvem.png" alt=""> -->
+    </div>
+     <main>
+      <a style="color: rgb(193, 167, 0);" href="index.php?name=CONSTRUTORA"> <h2 class="titles" >CONSTRUTORA</h2></a>
+      <a style="color:green;" href="index.php?name=GESTOR_DE_FAZENDAS"> <h2 class="titles">GESTOR DE FAZENDAS</h2></a>
+      <a href="index.php?name=GTS"> <h2 class="titles">GTS</h2></a>
+     </main>
--- index.php
+++ index.php
@@ -1,26 +1,43 @@
-<?php
+    <?php
+        include_once("header.php");
+       
+         $name = $_GET["name"];
+         #le o conteudo do arquivo em json
+         $json =  file_get_contents('exemplo.json');
+         #decodifica o JSON para um array associativo
+         $config = json_decode($json, true);
+        
+          $pasta = dir($name."/");
+          $diretorio = "../../../home/Documentos/".$name;
+          echo getcwd();
+            #verificacao se o caminho é um diretorio
+       if(is_dir($diretorio))
+         {
 
-$diretorio  = 'arquivo1';
-$pasta = dir("arquivo1/");
+            #obtem uma lista de arquivos  e diretorios no diretorio
+            $arquivos = scandir($diretorio);
 
-if(is_dir($diretorio)){
-    #teriamos aqui uma lista de arquivos  e diretorios no diretorio
-    $arquivos = scandir($diretorio);
-    #ja aqui remove os diretorios '.' e '..' da lista
-    $arquivos = array_diff($arquivos, array(',', '..'));
+            #ja aqui remove os diretorios '.' e '..' da lista
+            $arquivos = array_diff($arquivos, array('.', '..'));
 
-    //Bora interar agora a list de arquivos
-    //Estou tentando passar tanto a pasta quanto o nome do arquivo para ser aberto
-    foreach($arquivos as $arquivo){ ?>
-        <a href="<?php echo $pasta->path.$arquivo ?>"><?php
-        echo $arquivo;
-        ?></a>
-        <br>
-   <?php
-    }
-}else {
-    echo "O diretorio nao existe";
-}
+            //Bora interar agora a list de arquivos
+            //Estou tentando passar tanto a pasta quanto o nome do arquivo para ser aberto
+            
+            foreach($arquivos as $arquivo){ 
+    ?>
+            <br>
+               <a href="<?php echo $pasta->path.$arquivo ?>">
+    <?php
+               echo $arquivo;
+    ?>
+               </a>
+            <br>
+    <?php
+         }
+         }else 
+         {
+                echo "O diretorio nao existe";
+         }
 
-
-?>
+ include_once("footer.php");        
+    ?>
